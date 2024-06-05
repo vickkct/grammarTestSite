7e4a6270d63c00e1ac4763217d21c8d678bc41d9
Vickkct <kondratiewa.vick@yandex,ru>
2024-06-05 16:23:45 +0300

    add text rules

--- scriptPageRules.js
+++ scriptPageRules.js
@@ -16,10 +16,13 @@ const mainRule = document.getElementById("rule-block")
 const divTextRule = document.getElementById("rule-text-div")
 document.getElementById("exit-rule-btn").addEventListener('click', closeRule);
 
+var numberRule = 0;
+
 function openRule(event){
     mainListRules.classList.toggle("hidden");
     mainRule.classList.remove("hidden");
-    divTextRule.innerHTML = dataRules["rule2"];
+    console.log(dataRules[0])
+    divTextRule.innerHTML = dataRules[0]["ruleText"];
 }
 function closeRule(event){
     mainRule.classList.toggle("hidden");
